0cd4bcab0d08089389bb88347a92cf89f9fe234c
Unknown <dcutitoiu@yahoo.com>
2018-04-15 13:59:42 +0300

    Add a table like structure to display data from requests

--- DrivingSchool/public/javascripts/admin.js
+++ DrivingSchool/public/javascripts/admin.js
@@ -3,14 +3,32 @@ Vue.prototype.$http = axios;
 let app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
-        viewIndex : {
-            registerRequestView : 1,
-            instructorView : 2,
-            studentView : 3,
-            adminView : 4
+        viewIndex: {
+            registerRequestView: 1,
+            instructorView: 2,
+            studentView: 3,
+            adminView: 4
         },
-        currentView : 1
+        currentView: 1,
+        viewTitle: 'Inscrieri',
+        menuOptionText: {
+            request: 'Inscrieri',
+            instructor: 'Instructori',
+            student: 'Cursanti',
+            admin: 'Administratori'
+        },
+        headers: [
+            { text: 'Numar', width: 10 },
+            { text: 'Nume', width: 80 },
+            { text: 'Action', width: 10 }
+        ],
+        data: [
+            [1, 'Ion', 'Ion'],
+            [1, 'Ion', 'Ion'],
+            [1, 'Ion', 'Ion'],
+            [1, 'Ion', 'Ion']
+        ],
+        selectedRowIndex: null
     },
     methods: {
         onRegisterRequests: function () {
@@ -57,8 +75,12 @@ let app = new Vue({
                     console.log(err.response);
                 });
         },
-        enableView : function(viewIndex) {
+        enableView: function (viewIndex) {
             this.currentView = viewIndex;
+        },
+        selectRow(rowIndex) {
+            console.log(rowIndex);
+            this.selectedRowIndex = this.selectedRowIndex == rowIndex ? null : rowIndex;
         }
     }
 });
