f6897e176d600c94c78623dcc4ecec590a1ee765
Unknown <dcutitoiu@yahoo.com>
2018-05-10 00:29:55 +0300

    Add student routes. Update student controller

--- DrivingSchool/src/controllers/student.js
+++ DrivingSchool/src/controllers/student.js
@@ -1,126 +1,34 @@
 
 class StudentController {
     constructor(studentModel) {
-        this.students = studentModel;
-      }
-
-      getStudents(done) {
-        this.students.find({}, done);
-      }
-   /* getStudents (){
-        return [
-            {
-                lastName: 'Georgescu',
-                firstName: 'Andrei',
-                motherFirstName: 'Ioana',
-                fatherFirstName: 'Ion',
-                region: 'Dolj',
-                city: 'Craiova',
-                address: 'Strada AI Cuza, Bloc B, Sc. 4, Et. 1, Ap. 3 ',
-                cnp: '1900909095000',
-                idSeries: 'DZ',
-                idNumber: '2000',
-                dateOfBirth: '2017-07-04',
-                gender: 'male',
-                marriageStatus: 'married',
-                phone: '0722222222',
-                email: 'student@google.com',
-                password: 'password'
-            },
-            {
-                lastName: 'Georgescu',
-                firstName: 'Andrei',
-                motherFirstName: 'Ioana',
-                fatherFirstName: 'Ion',
-                region: 'Dolj',
-                city: 'Craiova',
-                address: 'Strada AI Cuza, Bloc B, Sc. 4, Et. 1, Ap. 3 ',
-                cnp: '1900909095000',
-                idSeries: 'DZ',
-                idNumber: '2000',
-                dateOfBirth: '2017-07-04',
-                gender: 'male',
-                marriageStatus: 'married',
-                phone: '0722222222',
-                email: 'student@google.com',
-                password: 'password'
-            },
-            {
-                lastName: 'Georgescu',
-                firstName: 'Andrei',
-                motherFirstName: 'Ioana',
-                fatherFirstName: 'Ion',
-                region: 'Dolj',
-                city: 'Craiova',
-                address: 'Strada AI Cuza, Bloc B, Sc. 4, Et. 1, Ap. 3 ',
-                cnp: '1900909095000',
-                idSeries: 'DZ',
-                idNumber: '2000',
-                dateOfBirth: '2017-07-04',
-                gender: 'male',
-                marriageStatus: 'married',
-                phone: '0722222222',
-                email: 'student@google.com',
-                password: 'password'
-            },
-            {
-                lastName: 'Georgescu',
-                firstName: 'Andrei',
-                motherFirstName: 'Ioana',
-                fatherFirstName: 'Ion',
-                region: 'Dolj',
-                city: 'Craiova',
-                address: 'Strada AI Cuza, Bloc B, Sc. 4, Et. 1, Ap. 3 ',
-                cnp: '1900909095000',
-                idSeries: 'DZ',
-                idNumber: '2000',
-                dateOfBirth: '2017-07-04',
-                gender: 'male',
-                marriageStatus: 'married',
-                phone: '0722222222',
-                email: 'student@google.com',
-                password: 'password'
-            }
-        ];
-    }*/
-    getArchivedStudents(done) {
-        return this.getStudents(done);
+        this.student = studentModel;
     }
-    /*
-    getScheduleForStudent(id) {
-        return [
-            {
-                date: '22/03/2018',
-                hour: '10:30',
-                location: 'Restaurant Minerva'
-            },
-            {
-                date: '22/03/2018',
-                hour: '10:30',
-                location: 'Restaurant Minerva'
-            },
-            {
-                date: '22/03/2018',
-                hour: '10:30',
-                location: 'Restaurant Minerva'
-            },
-            {
-                date: '22/03/2018',
-                hour: '10:30',
-                location: 'Restaurant Minerva'
-            }
-        ];
-    }*/
-    addStudent(student, done) {
-        let newStudent = new this.students(student);
-        newStudent.save(done);
-      }
-    
-      deleteStudent(cnp, done) {
-       
-        this.students.find({cnp: cnp}).remove()
-        .exec(done);
-      }
+
+    getStudents(done) {
+        this.student.find({}).exec(done);
+    }
+    getStudent(id, done) {
+        this.student.findById(id, done);
+    }
+    getArchivedStudents(done) {
+        this.student.find({ archived: true }).exec(done);
+    }
+    archiveStudent(id, done) {
+        this.student.findOneAndUpdate({ _id: id },
+            { $set: { archived: true } },
+            { new: true }, done);
+    }
+    unarchiveStudent(id, done) {
+        this.student.findOneAndUpdate({ _id: id }, 
+            { $set: { archived: false } }, 
+            { new: true }, done);
+    }
+    updateStudent(id, student, done) {
+        this.student.findOneAndUpdate({ _id: id }, student, { new: true }, done);
+    }
+    deleteStudent(id, done) {
+        this.student.findByIdAndRemove(id, done);
+    }
 }
 
 module.exports = StudentController;
